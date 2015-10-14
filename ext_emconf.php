d6f3ff3a159410456c8cde332ac522b91466d17e
Richard Bausek <office@artibella.com>
2015-10-14 13:50:47 +0200

    import from TER at version 0.2.2

---
+++ ext_emconf.php
@@ -0,0 +1,38 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "rb_flashobject".
+ *
+ * Auto generated 14-10-2015 13:47
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'SWFObject',
+	'description' => 'This plugin inserts a Flash movie to pages using the SWFObject Javascript method. Features: Flash detection, alternative HTML content, optional redirection, standards compilant, eliminates IE EULA problem, pass static or dynamic variables to Flash using FlashVars',
+	'category' => 'plugin',
+	'version' => '0.2.2',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'clearcacheonload' => 0,
+	'author' => 'Richard Bausek',
+	'author_email' => 'office@artibella.com',
+	'author_company' => '',
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+		),
+		'conflicts' => 
+		array (
+		),
+		'suggests' => 
+		array (
+		),
+	),
+);
+
